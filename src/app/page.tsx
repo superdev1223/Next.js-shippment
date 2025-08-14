29650100fee70f8e4bb3fbf046a45295572bc237
rabbitdev1223 <rabbitdev1223@gmail.com>
2025-08-14 21:25:24 +0900

    ignore typescript error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -39,7 +39,7 @@ export default function Home() {
     }));
   };
 
-  const openDetailsModal = (item) => {
+  const openDetailsModal = (item :) => {
     setSelectedItem(item);
     setShowDetailsModal(true);
   };
